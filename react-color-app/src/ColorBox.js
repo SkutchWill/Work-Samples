c10aa530e617b7e1cd9ed2b37f903ddbce0ad90e
Will Suttner <willsuttner@yahoo.com>
2021-12-21 22:06:23 -0800

    added copy functionality and color helpers functions

--- react-color-app/src/ColorBox.js
+++ react-color-app/src/ColorBox.js
@@ -1,17 +1,31 @@
-import React from 'react';
+import  React, { useState } from 'react';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 import './styles/ColorBox.css';
 
 export default function ColorBox(props) {
     const { name, background } = props;
+    const [copied, setCopy] = useState(false);
+
+    const changeCopyState = () => {
+        setCopy(true);
+        setTimeout(() => {setCopy(false)}, 1500);
+    }
     return (
-        <div style={{ background }}className='ColorBox'>
-            <div className='copy-container'>
-                <div className='box-content'>
-                    <span>{name}</span>
+        <CopyToClipboard text={background} onCopy={changeCopyState}>
+            <div style={{ background }} className='ColorBox'>
+                <div style={{ background }} className={`copy-overlay ${copied && 'show'}`}/>
+                <div className={`copy-message ${copied && 'show'}`}>
+                    <h1>copied!</h1>
+                    <p>{background}</p>
                 </div>
-                <button className='copy-button'>Copy</button>
+                <div className='copy-container'>
+                    <div className='box-content'>
+                        <span>{name}</span>
+                    </div>
+                    <button className='copy-button'>Copy</button>
+                </div>
+                <span className='see-more'>More</span>
             </div>
-            <span className='see-more'>More</span>
-        </div>
+        </CopyToClipboard>
     );
 }
